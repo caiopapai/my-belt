3084da9c571d8519dd6dfb1db5e84a31c56116a9
caiopapai <caio_papai@hotmail.com>
2020-03-14 18:18:18 -0300

    estrutura do projeto com spring-boot

---
+++ src/test/java/com/mybelt/MybeltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybelt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybeltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
